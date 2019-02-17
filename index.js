24ec5f65ffb7494272f01c5c2b16f4eafe21aab9
Mohammed Noori <kmanoorib@gmail.com>
2019-02-17 06:57:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ let newID = oldID + 1000000000;
 
 let ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.floor((Math.random() + 1));
+let randomNumber = Math.random() + 1;
 
 let randomInteger = Math.floor((Math.random() + 1));
 
-let randomUserID = Math.floor((Math.random() +))
-1000000001
+let randomUserID = Math.floor((Math.random() + 1000000000));
+
   
 
